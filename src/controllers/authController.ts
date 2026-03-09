bbd0e575e5176c4ef1b24a996506fe6b4febfc00
unknown <yuvrajsune02@gmail.com>
2026-03-09 08:48:23 +0530

    corrected controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,12 +2,13 @@ import { createUser } from '../models/userModal';
 import { Request, Response } from 'express';
 
 export const registerUser = async (req: Request, res: Response) => {
+  console.log('Received registration request with body:', req.body);
   try {
     const { name, email, password, role } = req.body;
     await createUser({ name, email, password, role });
-    debugger;
-    return res.status(201).json({ message: 'User created successfully' });
+    res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
-    return res.status(500).json({ message: 'Server error' });
+    console.log('Error in registerUser:', error);
+    res.status(500).json({ message: 'Server error' });
   }
 };
